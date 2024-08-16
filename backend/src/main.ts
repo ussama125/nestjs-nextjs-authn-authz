b56b2a32ccbdb84500d5d308ce0bd96abf3a8f06
Ussama Zubair <malikusama124@gmail.com>
2024-08-16 19:43:52 +0500

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   // Enable CORS
-  app.enableCors();
+  const allowedOrigins = [
+    'http://127.0.0.1',
+    // 'http://127.0.0.1:3000',
+    'http://localhost',
+    // 'http://localhost:3000'
+  ];
+
+  app.enableCors({
+    optionsSuccessStatus: 200,
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
