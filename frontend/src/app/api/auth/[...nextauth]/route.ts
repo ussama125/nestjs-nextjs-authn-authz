a11f47c999fc2e4eadad0edc57875c06a1484757
Ussama Zubair <malikusama124@gmail.com>
2024-08-18 15:19:48 +0500

    docker setup

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,18 +1,3 @@
-import { DecodedToken } from "@/types";
-import { jwtDecode } from "jwt-decode";
-
-export const validateToken = (token: string): boolean => {
-  try {
-    const decoded: DecodedToken = jwtDecode(token);
-    if (decoded.exp * 1000 < Date.now()) {
-      return false; // Token is expired
-    }
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-
 export const GetAccessToken = () => {
   if (typeof window !== "undefined") {
     return localStorage.getItem("token");
